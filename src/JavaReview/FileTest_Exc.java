fa86b04985067aa914ed98ce308eaa4c47705375
speedhawk1 <1010259701@qq.com>
2016-10-31 21:08:42 +0800

    Update

--- src/JavaReview/FileTest_Exc.java
+++ src/JavaReview/FileTest_Exc.java
@@ -23,6 +23,8 @@ public class FileTest_Exc {
     public static void main(String[] args) {
         try {
             BufferedReader br = new BufferedReader(new FileReader("D:/myproject/Homework20160908/FileUse/FileReader.txt"));
+            // BufferedReader与Reader的区别是前者将读入流缓冲而不直接释放，提高读写效率。
+            // BufferedReader的构造方法参数为一个Reader类值，作用是将Reader读入流存入堆栈区。
             BufferedWriter bw = new BufferedWriter(new FileWriter("D:/myproject/Homework20160908/FileUse/new_FileReader.txt"));
             /*Writer bw = new FileWriter("D:/myproject/Homework20160908/FileUse/FileReader.txt");*/
             int i;
