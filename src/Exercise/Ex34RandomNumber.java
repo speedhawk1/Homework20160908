a47c82119b4216ba500a7fd81fb351a9aba6894d
speedhawk1 <1010259701@qq.com>
2016-11-10 20:32:39 +0800

    Update

--- src/Exercise/Ex34RandomNumber.java
+++ src/Exercise/Ex34RandomNumber.java
@@ -7,13 +7,14 @@ import java.util.Hashtable;
  */
 public class Ex34RandomNumber {
     public static void main(String[] args) {
-        Hashtable<String,Integer> hash = new Hashtable<>();
+       /* Hashtable<String,Integer> hash = new Hashtable<>();
         hash.put("zhangsan",1);
         hash.put("lisi",2);
         hash.put("wangwu",3);
         hash.put("maliu",4);
 //        System.out.print(hash.isEmpty());
         System.out.println(hash.hashCode());
-        System.out.println(hash.keySet());
+        System.out.println(hash.keySet());*/
+
     }
 }
